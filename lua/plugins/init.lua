a5ddaa3833cbbf6b4c1d756b1841a45fe46f07bb
Ella Hinschen <ella@ellahin.com>
2024-10-14 09:11:32 +1100

    added telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -38,6 +38,9 @@ return {
   },
    {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+      config = function()
+        require "configs.telescope"
+      end,
+      dependencies = { 'nvim-lua/plenary.nvim' },
+  },
 }
