4ecfee15d48ef743ac99b568198634e0d3802ca1
Ella Hinschen <ella@ellahin.com>
2024-11-22 12:23:39 +1100

    updated lsp config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,5 +1,31 @@
 local lspconfig = require("lspconfig")
 local util = require "lspconfig/util"
+local M = {}
+local map = vim.keymap.set
+
+-- export on_attach & capabilities
+M.on_attach = function(_, bufnr)
+  local function opts(desc)
+    return { buffer = bufnr, desc = "LSP " .. desc }
+  end
+
+  map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
+  map("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
+  map("n", "gi", vim.lsp.buf.implementation, opts "Go to implementation")
+  map("n", "<leader>sh", vim.lsp.buf.signature_help, opts "Show signature help")
+  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
+  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
+
+  map("n", "<leader>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, opts "List workspace folders")
+
+  map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Go to type definition")
+  map("n", "<leader>ra", require "nvchad.lsp.renamer", opts "NvRenamer")
+
+  map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts "Code action")
+  map("n", "gr", vim.lsp.buf.references, opts "Show references")
+end
 
 require'lspconfig'.rust_analyzer.setup{
   root_dir = util.root_pattern("Cargo.toml"),
@@ -43,8 +69,15 @@ lspconfig.html.setup {
 }
 }
 lspconfig.clangd.setup{
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    M.on_attach(client, bufnr)
+  end,
+
 }
 lspconfig.pyright.setup{}
 lspconfig.sqlls.setup{}
 require'lspconfig'.arduino_language_server.setup{}
 require'lspconfig'.golangci_lint_ls.setup{}
+
+return M
